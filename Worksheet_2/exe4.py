b010a58efea7827c4d1dc47bc82b97a50c28a0bc
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-01 21:56:30 +0530

    Update exe4.py

--- Worksheet_2/exe4.py
+++ Worksheet_2/exe4.py
@@ -1,6 +1,5 @@
 def decimal_converter(number):
-    number=list(number)
-    print(number)
+    # print(number)
     sum=0
     number.reverse()
     for i in range(0,len(number)):
@@ -10,8 +9,17 @@ def decimal_converter(number):
 
 def main():
     binary_digits=input("Enter the binary digit numbers: ")
-    x=decimal_converter(binary_digits)
-    print(x)
+    num = list(binary_digits)
+    count_1s=num.count('1')
+    #print(count_1s)
+    count_0s=num.count('0')
+    #print(count_0s)
+    if (count_1s+count_0s)!=len(num):
+        print("Please enter only binary digit numbers: 0 or 1, Try again")
+        main()
+    else:
+        x = decimal_converter(num)
+        print(x)
 
 if __name__=="__main__":
     main()
