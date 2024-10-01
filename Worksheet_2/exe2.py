cdaa823bbb97f739610978b8172125e3e5ce21a1
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-01 19:16:39 +0530

    Create exe2.py

---
+++ Worksheet_2/exe2.py
@@ -0,0 +1,16 @@
+def binary_convert(quotient):
+    remainder = []
+    while quotient >=1 :
+        new_quotient=quotient//2
+        remainder.append(quotient%2)
+        quotient=new_quotient
+    return remainder
+
+def main():
+    number=int(input("Enter a decimal number: "))
+    x=binary_convert(number)
+    x.reverse()
+    print(x)
+
+if __name__=="__main__":
+    main()
