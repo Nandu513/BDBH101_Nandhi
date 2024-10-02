fe4e47b842f170c83c4dcf7e0d4d856b28c5089e
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-03 00:25:20 +0530

    Update exe5.py

--- Worksheet_2/exe5.py
+++ Worksheet_2/exe5.py
@@ -1,12 +1,18 @@
-def compute_power(x,y):
-    result=x**y
-    return result
+def check_prime(num):
+    i=num-1
+    while i > 1:
+        if num % i == 0:
+            return False
+        i -= 1
+    return True
 
 def main():
-    base=float(input("Enter the base: "))
-    power=float(input("Enter the power to be raised to: "))
-    result=compute_power(base,power)
-    print(result)
+    num=int(input("Enter a number to check if it is prime or not: "))
+    prime=check_prime(num)
+    if prime:
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
