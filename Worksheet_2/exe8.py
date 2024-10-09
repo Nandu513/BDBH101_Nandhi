a6ae3d1153750c920b1a15fe94498dd1f630077b
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-09 11:22:35 +0530

    Update exe8.py

--- Worksheet_2/exe8.py
+++ Worksheet_2/exe8.py
@@ -8,7 +8,7 @@ def string(s):
 def main():
     string_half=input("Enter a string: ")
     half_string=string(string_half)
-    print(half_string)
+    print("The half of the", string_half, "is", half_string)
 
 if __name__ == '__main__':
     main()
