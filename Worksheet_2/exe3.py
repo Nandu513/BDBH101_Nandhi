d98ca311d4e7e52b39521b9aa983e7a8726e977b
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-09 10:46:41 +0530

    Update exe3.py

--- Worksheet_2/exe3.py
+++ Worksheet_2/exe3.py
@@ -1,17 +1,15 @@
 def binary_convert(quotient):
-    remainder = []
+    remainder = ""
     while quotient >=1 :
         new_quotient=quotient//2
-        remainder.append(quotient%2)
+        remainder+=str((quotient%2))
         quotient=new_quotient
     return remainder
 
 def main():
     number=int(input("Enter a decimal number: "))
     x=binary_convert(number)
-    x.reverse()
-    print(x)
-    print(x.count(1))
+    print("The binary number of 1s in binary representation", x[::-1]," of", number, ": ", x[::-1].count('1'))
 
 if __name__=="__main__":
     main()
