9439a313f4ffd04721f5e5ba00445315e72b7895
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-09 10:41:24 +0530

    Update exe2.py

--- Worksheet_2/exe2.py
+++ Worksheet_2/exe2.py
@@ -1,19 +1,15 @@
 def binary_convert(quotient):
-    remainder = []
+    remainder = ""
     while quotient >=1 :
         new_quotient=quotient//2
-        remainder.append(quotient%2)
+        remainder+=str((quotient%2))
         quotient=new_quotient
     return remainder
 
 def main():
     number=int(input("Enter a decimal number: "))
-    if number==1 or number==0:
-        print(number)
-    else:
-        x=binary_convert(number)
-        x.reverse()
-        print(x)
+    x=binary_convert(number)
+    print(x[::-1], "is the binary number of", number)
 
 if __name__=="__main__":
     main()
