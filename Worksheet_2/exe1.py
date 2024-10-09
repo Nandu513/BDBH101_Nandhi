46a54e9dd3c615232712cad53fd19400b1614953
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-09 10:30:27 +0530

    Update exe1.py

--- Worksheet_2/exe1.py
+++ Worksheet_2/exe1.py
@@ -3,14 +3,17 @@ def squares(n):
     for i in range(1, n+1):
         square.append(i**2)
     return square
-def sum():
-    sum=0
-    num = int(input("Enter the range of number(like; 10 means for 1st 10 numbers): "))
-    square=squares(num)
-    print(square)
-    for i in range(0,len(square)):
-        sum+=square[i]
-    print (sum)
+def sum_sq():
+    sum_all=0
+    num = int(input( "Enter the range of number(like; 10 means for 1st 10 numbers): "))
+    if num<0:
+        print("Please enter a valid range, enter only positive number as input")
+    else:
+        square=squares(num)
+        print(square)
+        for i in range(0,len(square)):
+                sum_all+=square[i]
+        print (sum_all, "is the sum of all squares of first", num, "numbers")
 
 if __name__=="__main__":
-    sum()
+    sum_sq()
