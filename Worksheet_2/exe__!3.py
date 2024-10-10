7d137c38d76013ef8a335e87b8157846bae32c75
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-10 10:47:37 +0530

    Update exe__!3.py

--- Worksheet_2/exe__!3.py
+++ Worksheet_2/exe__!3.py
@@ -9,6 +9,8 @@ def main():
     ch_replace=input("Enter a character to replace with: ")
     if not str_input or not ch_find or not ch_replace:
         print("Invalid inputs, You have not entered all the required inputs")
+    elif str_input.count(ch_find)==0:
+        print (ch_find, "Character not found in given string", str_input)
     else:
         out_put=str_replace(str_input,ch_find,ch_replace)
         print("The output after replacing a character in given string is: ", out_put)
