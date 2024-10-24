a2142e58f4d3169890e04188bd15e86df75bf43f
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-24 12:48:13 +0530

    Create exe9.py

---
+++ Worksheet_2_python/exe9.py
@@ -0,0 +1,5 @@
+dict_all={1:'hi',2:"hello",3:"welcome"}
+print(f'Input: {dict_all}')
+print(f'Output: accessing each key and value pair')
+for key,val in dict_all.items():
+    print(f'{key} : {val}')
