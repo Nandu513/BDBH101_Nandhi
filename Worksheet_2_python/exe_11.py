d12d642c83208f5e2ffd133a136e7c139f83c8a6
NAGA NANDHI REDDY GUNREDDY <110474513+Nandu513@users.noreply.github.com>
2024-10-24 12:50:25 +0530

    Create exe_11.py

---
+++ Worksheet_2_python/exe_11.py
@@ -0,0 +1,5 @@
+dict_values_max={'one':1,'two':2,'three':3,'four':4,'five':5}
+print(f'Input: {dict_values_max}')
+print("Output:")
+print(f'The max value in the given dict is: {max(dict_values_max.values())}')
+print(f'The min value in the given dict is: {min(dict_values_max.values())}')
